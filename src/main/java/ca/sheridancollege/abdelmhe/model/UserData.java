d0a9a679a5fc03a57dcbe93fed5e85d1b6d3862b
abdelmhe <abdelmhe@sheridancollege.ca>
2021-02-13 05:49:54 +0200

    the user data class has the following fields game options, option,computer option and result, getgameoption returns gameoption, set gameoption sets whether it stone, paper or scissors, getcomputeroption returns computeroption, set computer option sets the coputer option, get result returns the result, set result sets the result

--- src/main/java/ca/sheridancollege/abdelmhe/model/UserData.java
+++ src/main/java/ca/sheridancollege/abdelmhe/model/UserData.java
@@ -1,4 +1,36 @@
 package ca.sheridancollege.abdelmhe.model;
 
+
+import ca.sheridancollege.abdelmhe.GameOptions;
+
 public class UserData {
+    private GameOptions _gameOption;
+    private int option;
+    private String _ComputerOption;
+    private String _Result;
+
+    public GameOptions get_gameOption() {
+        return _gameOption;
+    }
+
+    public void set_gameOptions(GameOptions _gameOption) {
+        this._gameOption = _gameOption;
+    }
+
+    public String get_ComputerOption() {
+        return _ComputerOption;
+    }
+
+    public void set_ComputerOption(String _ComputerOption) {
+        this._ComputerOption = _ComputerOption;
+    }
+
+    public String get_Result() {
+        return _Result;
+    }
+
+    public void set_Result(String _Result) {
+        this._Result = _Result;
+    }
 }
+
