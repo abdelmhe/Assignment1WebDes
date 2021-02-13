fbe6690584298272380af4bd0cf2471a225b5c6c
abdelmhe <abdelmhe@sheridancollege.ca>
2021-02-13 05:51:49 +0200

    This is where my application is going to run from

--- src/main/java/ca/sheridancollege/abdelmhe/RockpaperscissorsApplication.java
+++ src/main/java/ca/sheridancollege/abdelmhe/RockpaperscissorsApplication.java
@@ -1,4 +1,14 @@
 package ca.sheridancollege.abdelmhe;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RockpaperscissorsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RockpaperscissorsApplication.class, args);
+    }
+
 }
