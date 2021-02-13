ff15eb4483631b22cb9beecc6e747f2363902b65
abdelmhe <abdelmhe@sheridancollege.ca>
2021-02-13 06:18:44 +0200

    Here I am assigning the randomly picked option to the model above. I demonstrated the different user scenarios via using switch and case. These different cases are Case when user selected Scissor option,Case when user selected Paper option, and Case when user selected Stone option

--- src/main/java/ca/sheridancollege/abdelmhe/controllers/GameController.java
+++ src/main/java/ca/sheridancollege/abdelmhe/controllers/GameController.java
@@ -2,6 +2,7 @@ package ca.sheridancollege.abdelmhe.controllers;
 
 import java.util.Random;
 
+import ca.sheridancollege.abdelmhe.GameOptions;
 import ca.sheridancollege.abdelmhe.model.UserData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -27,5 +28,47 @@ public class GameController {
         Random r = new Random();
         String comp = list[r.nextInt(list.length)];
         userData.set_ComputerOption(comp);
+        /*Assign the randomly picked option to model above*/
 
-    }
+        switch(_gameOption) {
+            //Case when user selected Scissor option
+            case "Scissor":
+                userData.set_gameOptions(GameOptions.Scissor);
+                if(userData.get_ComputerOption().equals("Paper")) {
+                    userData.set_Result("Scissor cuts paper. You win!!");
+                }
+                else if(userData.get_ComputerOption().equals("Stone")) {
+                    userData.set_Result("Rock crushes scissors. You lose!!");
+                }
+                else if(userData.get_ComputerOption().equals("Scissor")) {
+                    userData.set_Result("It's a tie!");
+                }
+                break;
+            case "Paper":
+                //Case when user selected Paper option
+                userData.set_gameOptions(GameOptions.Paper);
+                if(userData.get_ComputerOption().equals("Paper")) {
+                    userData.set_Result("It's a tie!");
+                }
+                else if(userData.get_ComputerOption().equals("Stone")) {
+                    userData.set_Result("Paper eats rock. You win!!");
+                }
+                else if(userData.get_ComputerOption().equals("Scissor")) {
+                    userData.set_Result("Scissor cuts paper. You lose!!");
+                }
+                break;
+            case "Stone":
+                //Case when user selected Stone option
+                userData.set_gameOptions(GameOptions.Stone);
+                if(userData.get_ComputerOption().equals("Paper")) {
+                    userData.set_Result("Paper eats rock. You lose!!");
+                }
+                else if(userData.get_ComputerOption().equals("Stone")) {
+                    userData.set_Result("It's a tie!");
+                }
+                else if(userData.get_ComputerOption().equals("Scissor")) {
+                    userData.set_Result("Rock crushes scissors. You win!!");
+                }
+                break;
+        }
+
